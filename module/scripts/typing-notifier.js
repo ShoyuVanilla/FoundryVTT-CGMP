a416cf2982c57f0617dd9444ddfeea8c159dfaea
Kyuho Lee <modulo641@gmail.com>
2020-05-27 09:27:53 +0900

    HOTFIX: private fields are to early

--- module/scripts/typing-notifier.js
+++ module/scripts/typing-notifier.js
@@ -8,31 +8,31 @@ const REMOTE_TYPING_TIMEOUT = 5000;
 const TYPING_EMIT_INTERVAL = 200;
 
 export class TypingNotifier {
-	#notifyWrapperElement;
-	#notifyDiv;
-	#typingUsers;
-	#isNoticeVisible;
-	#lastPacketSent;
+	_notifyWrapperElement;
+	_notifyDiv;
+	_typingUsers;
+	_isNoticeVisible;
+	_lastPacketSent;
 
 	constructor() {
-		this.#typingUsers = new Map();
-		this.#isNoticeVisible = false;
+		this._typingUsers = new Map();
+		this._isNoticeVisible = false;
 		this._render();
 		game.socket.on('module.CautiousGamemastersPack', data => this._onRemotePacket(data));
 	}
 
 	_render() {
-		this.#notifyWrapperElement = document.createElement("div");
-		this.#notifyWrapperElement.className = "typing-notify hidden";
+		this._notifyWrapperElement = document.createElement("div");
+		this._notifyWrapperElement.className = "typing-notify hidden";
 		let element = document.getElementById("chat-controls");
-		element.appendChild(this.#notifyWrapperElement);
+		element.appendChild(this._notifyWrapperElement);
 		let innerWrapper = document.createElement("div");
 		innerWrapper.innerHTML = '<span class="dots-cont"><span class="dot dot-1"></span><span class="dot dot-2"></span><span class="dot dot-3"></span></span>';
-		this.#notifyWrapperElement.appendChild(innerWrapper);
-		this.#notifyDiv = document.createElement("div");
-		this.#notifyDiv.className = "notify-text";
-		innerWrapper.appendChild(this.#notifyDiv);
-		this.#isNoticeVisible = false;
+		this._notifyWrapperElement.appendChild(innerWrapper);
+		this._notifyDiv = document.createElement("div");
+		this._notifyDiv.className = "notify-text";
+		innerWrapper.appendChild(this._notifyDiv);
+		this._isNoticeVisible = false;
 	}
 
 	_emitTypingEnd() {
@@ -40,26 +40,26 @@ export class TypingNotifier {
 			header: PACKET_HEADER.TYPING_END,
 			user: game.user.id
 		});
-		this.#lastPacketSent = undefined;
+		this._lastPacketSent = undefined;
 	}
 
 	_emitTyping() {
-		if (this.#lastPacketSent && new Date().getTime - this.#lastPacketSent < TYPING_EMIT_INTERVAL) return;
+		if (this._lastPacketSent && new Date().getTime - this._lastPacketSent < TYPING_EMIT_INTERVAL) return;
 		game.socket.emit('module.CautiousGamemastersPack', {
 			header: PACKET_HEADER.TYPING_MESSAGE,
 			user: game.user.id
 		});
-		this.#lastPacketSent = new Date().getTime;
+		this._lastPacketSent = new Date().getTime;
 	}
 
 	_setVisible(visible) {
-		if (!this.#notifyWrapperElement || visible === this.#isNoticeVisible) return;
-		this.#isNoticeVisible = visible;
-		this.#notifyWrapperElement.classList.toggle('hidden');
+		if (!this._notifyWrapperElement || visible === this._isNoticeVisible) return;
+		this._isNoticeVisible = visible;
+		this._notifyWrapperElement.classList.toggle('hidden');
 	}
 
 	_onRemoteTypingEnded(id) {
-		this.#typingUsers.delete(id);
+		this._typingUsers.delete(id);
 		this.updateNotice();
 	}
 
@@ -79,13 +79,13 @@ export class TypingNotifier {
 		switch (data.header) {
 			case PACKET_HEADER.TYPING_MESSAGE:
 				let pushed = true;
-				if (this.#typingUsers.has(id)) { pushed = false; clearTimeout(this.#typingUsers.get(id)); }
-				this.#typingUsers.set(id,
+				if (this._typingUsers.has(id)) { pushed = false; clearTimeout(this._typingUsers.get(id)); }
+				this._typingUsers.set(id,
 					setTimeout(() => this._onRemoteTypingEnded(id), REMOTE_TYPING_TIMEOUT));
 				if (pushed) this.updateNotice();
 				break;
 			case PACKET_HEADER.TYPING_END:
-				if (this.#typingUsers.has(id)) clearTimeout(this.#typingUsers.get(id));
+				if (this._typingUsers.has(id)) clearTimeout(this._typingUsers.get(id));
 				this._onRemoteTypingEnded(id);
 				break;
 			default:
@@ -94,8 +94,8 @@ export class TypingNotifier {
 	}
 
 	updateNotice() {
-		if (!this.#notifyWrapperElement) return;
-		const mapSize = this.#typingUsers.size;
+		if (!this._notifyWrapperElement) return;
+		const mapSize = this._typingUsers.size;
 		if (mapSize === 0) {
 			this._setVisible(false);
 			return;
@@ -105,7 +105,7 @@ export class TypingNotifier {
 		let cnt = 0;
 		let users = [];
 
-		this.#typingUsers.forEach((value, key) => {
+		this._typingUsers.forEach((value, key) => {
 			users.push(game.users.get(key).name);
 			cnt++;
 		});
@@ -117,7 +117,7 @@ export class TypingNotifier {
 		} else {
 			text = game.i18n.format("cgmp.typing-many", { user1: users[0], user2: users[1], others: cnt - 2 });
 		}
-		this.#notifyDiv.innerHTML = text;
+		this._notifyDiv.innerHTML = text;
 
 		this._setVisible(true);
 	}
