11e709e4c3bd33bd910dd9e691fc116173493a15
Alan Davies <alan.n.davies@gmail.com>
2021-07-05 18:37:04 +0100

    Added copyright notices to source files

--- module/scripts/main.js
+++ module/scripts/main.js
@@ -1,3 +1,16 @@
+/*
+ * Cautious Gamemasters Pack
+ * https://github.com/cs96and/FoundryVTT-CGMP
+ *
+ * Copyright (c) 2020 Shoyu Vanilla - All Rights Reserved.
+ * Copyright (c) 2021 Alan Davies - All Rights Reserved.
+ *
+ * You may use, distribute and modify this code under the terms of the MIT license.
+ *
+ * You should have received a copy of the MIT license with this file. If not, please visit:
+ * https://mit-license.org/
+ */
+
 import { CGMPSettings, CGMP_OPTIONS } from "./settings.js";
 import { TypingNotifier } from "./typing-notifier.js";
 import { ChatResolver } from "./chat-resolver.js";
